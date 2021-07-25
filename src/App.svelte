4a10861c0ac17effb17c56fd567fcf3853a64079
Whimsicott <eruhoon@gmail.com>
2021-07-26 02:03:12 +0900

    fix error

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import ChatPage from "./view/chat/ChatPage.svelte";
-  import InvalidSecretKeyErrorPage from "./error/InvalidSecretKeyErrorPage.svelte";
+  import InvalidSecretKeyErrorPage from "./view/error/InvalidSecretKeyErrorPage.svelte";
 
   const urlParams = new URLSearchParams(window.location.search);
   const secretKey: string | null = urlParams.get("key") || null;
