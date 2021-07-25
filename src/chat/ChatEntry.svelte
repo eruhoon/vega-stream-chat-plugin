49456b9d39b2392ee2a1753a6dd6c55a19760d96
Whimsicott <eruhoon@gmail.com>
2021-07-26 01:00:12 +0900

    add ChatEntry

---
+++ src/chat/ChatEntry.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let nickname = "unknown";
+  export let message = "";
+</script>
+
+<div>
+  <div>{nickname}</div>
+  <div>{message}</div>
+</div>
--- src/chat/ChatList.svelte
+++ src/chat/ChatList.svelte
@@ -1,1 +1,11 @@
-<div>ChatList</div>
+<script>
+  import ChatEntry from "./ChatEntry.svelte";
+
+  const chats = [{ nickname: "user1", message: "chat1" }];
+</script>
+
+<div>
+  {#each chats as chat}
+    <ChatEntry nickname={chat.nickname} message={chat.message} />
+  {/each}
+</div>
